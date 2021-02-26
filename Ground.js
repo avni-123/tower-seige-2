de89e0bd7b2f9e0b15ada6dae7738c483cc35d8a
avni-123 <76476978+avni-123@users.noreply.github.com>
2021-02-26 14:31:14 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,25 @@
+class Ground{
+
+    constructor(){
+
+        var ground_options = {
+
+            isStatic : true
+
+          }
+        
+          this.ground = Bodies.rectangle(450, 390, 900, 20, ground_options);
+          World.add(world,this.ground);
+
+    }
+
+    display(){
+
+        noStroke();
+        fill(200, 50, 100);
+        rectMode(CENTER);
+        rect(this.ground.position.x, this.ground.position.y, 900, 20);
+
+    }
+
+}
